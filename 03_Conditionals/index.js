aee087b42a956cdf846cb1e4b6257b5281de8a16
BilguunTu <Beck.tumurbaator@gmail.com>
2024-05-20 20:04:03 +0800

    operators

---
+++ 03_Conditionals/index.js
@@ -0,0 +1,12 @@
+let num = 7;
+
+if (num > 2) {
+  console.log(`${num} настай сурагч`);
+}
+
+let nums = 9;
+if (nums >= 7) {
+  console.log(`${nums} настай сурагч`);
+} else {
+  console.log(`${7 - nums} жилийн дараа сургуульд орно`);
+}
